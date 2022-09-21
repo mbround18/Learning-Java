99a093e433e614ed55b6056ab4c039ff25767611
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2022-09-21 11:54:32 +0100

    Hello word Program
    First Program

---
+++ helloWorld/src/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ This is the comment command Description at the top
+ */
+package helloworld; // This is the package
+
+/*
+Name: Thomas Smith
+Location: Edinburgh College - Sighthill
+Date: 05/09/2022
+Version 1.0
+Notes: N/A
+ */
+public class HelloWorld 
+{
+
+    public static void main(String[] args) 
+    {
+      System.out.println("Hello World"); // Print statement in Java with a line return
+      
+      System.exit(0); // This will end the program and flush the ram
+    }
+    
+}
