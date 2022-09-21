eb8163540c8651f5e949e63cabff5f63eb7f0cbc
Blade-iii <54221403+Blade-iii@users.noreply.github.com>
2022-09-21 13:16:43 +0100

    Week 1 Practice Programs
    Introduction to Java week 1

---
+++ Week 01/starPyramid/src/starpyramid/StarPyramid.java
@@ -0,0 +1,26 @@
+/*
+ Create a star pyramid 
+ */
+package starpyramid;
+
+/*
+Name: Thomas Smith
+Location: Edinburgh College - Sighthill
+Date: 05/09/2022
+Version 1.0
+Notes: N/A
+ */
+public class StarPyramid 
+{
+
+    public static void main(String[] args) 
+    {
+      System.out.println("             "); // Print statement in Java with a line return
+      System.out.println("       *       "); // Print statement in Java with a line return
+      System.out.println("      ***       "); // Print statement in Java with a line return
+      System.out.println("     *****       "); // Print statement in Java with a line return
+      
+      System.exit(0); // This will end the program and flush the ram
+    }
+    
+}
---
+++ Week 01/stringMyName/src/stringmyname/StringMyName.java
@@ -0,0 +1,26 @@
+/*
+ Welcome message with a string variable
+ */
+package stringmyname;
+
+/*
+Name: Thomas Smith
+Location: Edinburgh College - Sighthill
+Date: 05/09/2022
+Version 1.0
+Notes: N/A
+ */
+public class StringMyName 
+{
+
+   
+    public static void main(String[] args) 
+    {
+    String fullName = "Thomas Smith"; // This is a string variable declared 
+      
+    System.out.println("Welcome " + fullName + " to Edinburgh College");
+    
+    System.exit(0);
+    }
+    
+}
